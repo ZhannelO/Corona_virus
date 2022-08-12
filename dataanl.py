275c0cb0b96a02c6e7e8c9475ce07f37970f1d1a
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-08-12 11:01:37 +0600

    Update dataanl.py

--- dataanl.py
+++ dataanl.py
@@ -7,9 +7,32 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 # for time
 from datetime import datetime,date,time,timezone
+import folium
 df=pd.read_csv("D:/corona_data/covid-v-data.csv")
 df['Date'] = pd.to_datetime(df['Day']) #convert string Date time into Python Date time object
 df.set_index('Date',inplace=True) 
 df.drop(['Day'],axis=1,inplace=True) #убирает данный столбец из данных
 # print(df.head())
-print(len(df['Entity'].unique()))
+map_osm=folium.Map(location = [43.17638980564814, 76.93068214581893],
+zoom_start=13,
+tiles="stamenwatercolor",
+)
+folium.Marker(
+   location= [43.17638980564814, 76.93068214581893],
+    popup="Your location",
+    icon=folium.Icon(icon="cloud"),
+).add_to(map_osm)
+folium.Marker(
+   location= [43.17707032518363, 76.82674114506969], 
+   popup="TimberLine Lodge", 
+   icon=folium.Icon(color="green"),
+).add_to(map_osm)
+folium.CircleMarker(
+    location=[43.17638980564814, 76.93068214581893],
+    radius=100,
+    popup="circle",
+    color='#3186cc',
+    fill=True,
+    fill_color='#3186cc',
+).add_to(map_osm)
+map_osm.save('new.html')
